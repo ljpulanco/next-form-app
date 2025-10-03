98c7fb55ee5cdf96ecd5fb43a3dd731cb644377b
Lester John Pulanco <lesterjohnpulanco@gmail.com>
2025-10-03 23:22:26 +0800

    fixed bug deployment issues

---
+++ src/declarations.d.ts
@@ -0,0 +1,26 @@
+declare module "react-signature-canvas" {
+  import * as React from "react";
+
+  export interface SignatureCanvasProps {
+    penColor?: string;
+    backgroundColor?: string;
+    canvasProps?: React.CanvasHTMLAttributes<HTMLCanvasElement>;
+    velocityFilterWeight?: number;
+    minWidth?: number;
+    maxWidth?: number;
+    dotSize?: number | (() => number);
+    throttle?: number;
+    onBegin?: () => void;
+    onEnd?: () => void;
+    clearOnResize?: boolean;
+  }
+
+  export default class SignatureCanvas extends React.Component<
+    SignatureCanvasProps
+  > {
+    clear(): void;
+    isEmpty(): boolean;
+    fromDataURL(base64: string): void;
+    toDataURL(type?: string, encoderOptions?: number): string;
+  }
+}
